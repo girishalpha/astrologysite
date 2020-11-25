38c6a0e2ba9395cacd63ab6e07c5fbfaf772f2b4
stazVLight <arvindh.pandiyan25121999@gmail.com>
2020-11-25 13:44:39 +0530

    all page upadtes 3 for right click disabled

---
+++ js/RightClick.js
@@ -0,0 +1,8 @@
+var el_up = document.getElementById("GFG_UP");
+var el_down = document.getElementById("GFG_DOWN");
+el_up.innerHTML = "Click on the button to disable right click";
+
+function gfg_Run() {
+  document.addEventListener("contextmenu", (event) => event.preventDefault());
+  el_down.innerHTML = "Right click disabled";
+}
