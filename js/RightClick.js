a62a306dee5737b26f55162a25056a077a8d402c
stazVLight <arvindh.pandiyan25121999@gmail.com>
2020-12-09 17:43:36 +0530

    update 12-09-2020

--- js/RightClick.js
+++ js/RightClick.js
@@ -3,6 +3,8 @@ var el_down = document.getElementById("GFG_DOWN");
 el_up.innerHTML = "Click on the button to disable right click";
 
 function gfg_Run() {
+  oneTwo();
   document.addEventListener("contextmenu", (event) => event.preventDefault());
   el_down.innerHTML = "Right click disabled";
+  
 }
